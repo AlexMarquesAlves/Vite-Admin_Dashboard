04f3b67c88048c8c9d632294b28cc3e3f40517ce
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-08 19:57:04 -0300

    Feat: Coding Routes

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -1,15 +1,18 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+import { Route, Routes as Switch } from "react-router-dom";
 
+import Dashboard from "../../pages/Dashboard";
+import Customers from "../../pages/Customers";
 interface RoutesProps {
   children: ReactNode;
 }
 
 function Routes({ children }: RoutesProps) {
   return (
-    <>
-      <h1>Routes</h1>
-      {children}
-    </>
+    <Switch>
+      <Route path="/" element={Dashboard} />
+      <Route path="/customers" element={Customers} />
+    </Switch>
   );
 }
 
