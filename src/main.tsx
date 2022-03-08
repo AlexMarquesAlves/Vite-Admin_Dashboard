df6f8a4b3fc9bbfbde43b288c36ef3809810fe52
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-08 19:46:12 -0300

    Feat: Cleaing up!

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
+import Layout from "./components/Layout";
+// Styles imports
+import "./assets/boxicons-2.0.7/css/boxicons.min.scss";
+import "./assets/css/grid.scss";
+import "./assets/css/index.scss";
+
+document.title = "¡Duque CRM";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById("root")
 );
