e49dce7e456506dfbca6e14e86eca1b82d22bcc0
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-08 19:57:55 -0300

    Feat: Sidebar Component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+import './styles.scss';
+
+interface SidebarProps {
+  children: ReactNode;
+}
+
+function Sidebar({ children }: SidebarProps) {
+  return (
+    <>
+      <h1>Sidebar</h1>
+      {children}
+    </>
+  );
+};
+
+export default Sidebar;
