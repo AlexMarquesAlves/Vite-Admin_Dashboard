8b81c40ee789e3d43d72dc949f09b924ae9ee74d
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-09 13:51:05 -0300

    Feat: Add Apexcharts on Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,55 @@
 import { ReactNode } from "react";
 import statusCards from "../../assets/JsonData/status-card-data.json";
 import StatusCard from "../../components/StatusCard";
+import Chart from "react-apexcharts";
 
 interface DashboardProps {
   children: ReactNode;
 }
+
+const chartOption: any = {
+  series: [
+    {
+      name: "Online Customers",
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60],
+    },
+    {
+      name: "Store Customers",
+      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10],
+    },
+  ],
+  options: {
+    color: ["#6ab04c", "#2980b9"],
+    chart: {
+      background: "transparent",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Fev",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+      ],
+    },
+    legend: {
+      position: "top",
+    },
+    grid: {
+      show: false,
+    },
+  },
+};
 
 function Dashboard({ children }: DashboardProps) {
   return (
@@ -26,7 +71,15 @@ function Dashboard({ children }: DashboardProps) {
           </div>
 
           <div className="col-6">
-            <div className="card full-height">{/* chart */}</div>
+            <div className="card full-height">
+              {/* chart */}
+              <Chart
+                options={chartOption.options}
+                series={chartOption.series}
+                type="line"
+                height="100%"
+              />
+            </div>
           </div>
         </div>
       </h2>
