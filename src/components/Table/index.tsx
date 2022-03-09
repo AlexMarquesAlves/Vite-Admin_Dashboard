f4c5ac62bcf4e8964c5c7ce35314e0a2b8c9fd00
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-09 20:43:03 -0300

    Feat: Coding Table component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -6,12 +6,26 @@ interface TableProps {
   children?: ReactNode;
 }
 
-function Table({ children }: TableProps) {
+function Table({ props }: TableProps) {
   return (
-    <>
-      <div>Table</div>
-      {children}
-    </>
+    <div className="table-wrapper">
+      <table>
+        {props.headData && props.renderHeader ? (
+          <thead>
+            <tr>
+              {props.headData.map((item, index) =>
+                props.renderHead(item, index)
+              )}
+            </tr>
+          </thead>
+        ) : null}
+        {props.bodyData && props.renderBody ? (
+          <tbody>
+            {props.bodyData.map((item, index) => props.renderBody(item, index))}
+          </tbody>
+        ) : null}
+      </table>
+    </div>
   );
 }
 
