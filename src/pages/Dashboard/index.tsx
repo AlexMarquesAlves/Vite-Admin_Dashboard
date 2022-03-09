f305a1688f7190439b40b8fbb28524f709d9df88
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-09 13:19:15 -0300

    Feat: Coding Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+import statusCards from "../../assets/JsonData/status-card-data.json";
+import StatusCard from "../../components/StatusCard";
 
 interface DashboardProps {
   children: ReactNode;
@@ -6,10 +8,25 @@ interface DashboardProps {
 
 function Dashboard({ children }: DashboardProps) {
   return (
-    <>
-      <h1>Dashboard</h1>
-      {children}
-    </>
+    <div>
+      <h2 className="page-header">
+        <div className="row">
+          <div className="col-6">
+            {statusCards.map((item, index) => (
+              <div className="col-6">
+                {/* status card here */}
+                {item.title}
+                <StatusCard
+                  icon={item.icon}
+                  count={item.count}
+                  title={item.title}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </h2>
+    </div>
   );
 }
 
