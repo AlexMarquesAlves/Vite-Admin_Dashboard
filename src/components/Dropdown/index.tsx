73dd9e86716ff8989a7e1cde91b89062795718c7
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-08 23:13:45 -0300

    Feat: Update TopNav Component

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,21 +1,22 @@
+import { useRef } from "react";
 import { ReactChild, ReactFragment, ReactNode, ReactPortal } from "react";
 
 import "./styles.scss";
 
 interface DropdownProps {
   children?: ReactNode;
-  icon: string | undefined;
-  badge: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined;
-  customToggle: () =>
+  icon?: string | undefined;
+  badge?: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined;
+  customToggle?: () =>
     | boolean
     | ReactChild
     | ReactFragment
     | ReactPortal
     | null
     | undefined;
-  contentData: any[];
+  contentData?: any[];
   renderItems: (arg0: any, arg1: any) => any;
-  renderFooter: () =>
+  renderFooter?: () =>
     | boolean
     | ReactChild
     | ReactFragment
@@ -24,9 +25,28 @@ interface DropdownProps {
     | undefined;
 }
 
+const clickOutsideRef = (content_ref: any, toggle_ref: any) => {
+  document.addEventListener("mousedown", (e) => {
+    // user click toggle
+    if (toggle_ref.current && toggle_ref.current.contains(e.target)) {
+      content_ref.current.classList.toggle("active");
+    } else {
+      //user click outside toggle and content
+      if (content_ref.current && !content_ref.current.contains(e.target)) {
+        content_ref.current.classList.remove("active");
+      }
+    }
+  });
+};
+
 function Dropdown(props: DropdownProps) {
+  const dropdown_toggle_el = useRef(null);
+  const dropdown_content_el = useRef(null);
+
+  clickOutsideRef(dropdown_content_el, dropdown_toggle_el);
+
   return (
-    <div className="dropdown">
+    <div ref={dropdown_toggle_el} className="dropdown">
       <button className="dropdown__toggle">
         {props.icon ? <i className={props.icon}></i> : ""}
         {props.badge ? (
@@ -36,7 +56,7 @@ function Dropdown(props: DropdownProps) {
         )}
         {props.customToggle ? props.customToggle() : ""}
       </button>
-      <div className="dropdown__content">
+      <div ref={dropdown_content_el} className="dropdown__content">
         {props.contentData && props.renderItems
           ? props.contentData.map((item, index) =>
               props.renderItems(item, index)
--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -1,10 +1,57 @@
-import { ReactNode } from "react";
+import { Key, ReactChild, ReactFragment, ReactNode, ReactPortal } from "react";
+import Dropdown from "../Dropdown";
+import notifications from "../../assets/JsonData/notification.json";
+import user_image from "../../assets/images/tuat.png";
+import user_menu from "../../assets/JsonData/user_menus.json";
 
 import "./styles.scss";
+import { Link } from "react-router-dom";
 
 interface TopNavProps {
   children?: ReactNode;
 }
+
+const curr_user = {
+  display_name: `¡Duque`,
+  image: user_image,
+};
+
+const renderNotifications = (
+  item: {
+    icon: string | undefined;
+    content:
+      | boolean
+      | ReactChild
+      | ReactFragment
+      | ReactPortal
+      | null
+      | undefined;
+  },
+  index: Key | null | undefined
+) => (
+  <div className="notification-item" key={index}>
+    <i className={item.icon}></i>
+    <span>{item.content}</span>
+  </div>
+);
+
+const renderUserToggle = (user) => (
+  <div className="topnav__right-user">
+    <div className="topnav__right-user__image">
+      <img src={user.image} alt="User pic" />
+    </div>
+    <div className="topnav__right-user__name">{user.display_name}</div>
+  </div>
+);
+
+const renderUserMenu = (item, index) => (
+  <Link to="/" key={index}>
+    <div className="notification-item">
+      <i className={item.icon}></i>
+      <span>{item.content}</span>
+    </div>
+  </Link>
+);
 
 function TopNav({ children }: TopNavProps) {
   return (
@@ -14,9 +61,28 @@ function TopNav({ children }: TopNavProps) {
         <i className="bx bx-search"></i>
       </div>
       <div className="topnav__right">
-        <div className="topnav__right-item">{/* Dropdown here */}</div>
-        <div className="topnav__right-item">{/* Dropdown here */}</div>
-        <div className="topnav__right-item">{/* theme settings */}</div>
+        <div className="topnav__right-item">
+          <Dropdown
+            customToggle={() => renderUserToggle(curr_user)}
+            contentData={user_menu}
+            renderItems={(item, index) => renderUserMenu(item, index)}
+          />
+          {/* Dropdown here */}
+        </div>
+        <div className="topnav__right-item">
+          <Dropdown
+            icon="bx bx-bell"
+            badge="12"
+            contentData={notifications}
+            renderItems={(item, index) => renderNotifications(item, index)}
+            renderFooter={() => <Link to="/">View All</Link>}
+          />
+          {/* Dropdown here */}
+        </div>
+        <div className="topnav__right-item">
+          <Dropdown />
+          {/* theme settings */}
+        </div>
       </div>
     </div>
   );
