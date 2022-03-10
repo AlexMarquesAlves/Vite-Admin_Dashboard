97b418b57b5dcae23a5a587b82cdbce1bacea9e6
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-10 13:10:30 -0300

    Feat: Make Customers page

--- src/pages/Customers/index.tsx
+++ src/pages/Customers/index.tsx
@@ -1,16 +1,116 @@
-import { ReactNode } from 'react';
+import React from "react";
 
-interface CustomersProps {
-  children: ReactNode;
-}
+import Table from "../../components/Table";
 
-function Customers({ children }: CustomersProps) {
+import customerList from "../../assets/JsonData/customers-list.json";
+
+const customerTableHead = [
+  "",
+  "name",
+  "email",
+  "phone",
+  "total orders",
+  "total spend",
+  "location",
+];
+
+const renderHead = (
+  item:
+    | boolean
+    | React.ReactChild
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined,
+  index: React.Key | null | undefined
+) => <th key={index}>{item}</th>;
+
+const renderBody = (
+  item: {
+    id:
+      | boolean
+      | React.ReactChild
+      | React.ReactFragment
+      | React.ReactPortal
+      | null
+      | undefined;
+    name:
+      | boolean
+      | React.ReactChild
+      | React.ReactFragment
+      | React.ReactPortal
+      | null
+      | undefined;
+    email:
+      | boolean
+      | React.ReactChild
+      | React.ReactFragment
+      | React.ReactPortal
+      | null
+      | undefined;
+    phone:
+      | boolean
+      | React.ReactChild
+      | React.ReactFragment
+      | React.ReactPortal
+      | null
+      | undefined;
+    total_orders:
+      | boolean
+      | React.ReactChild
+      | React.ReactFragment
+      | React.ReactPortal
+      | null
+      | undefined;
+    total_spend:
+      | boolean
+      | React.ReactChild
+      | React.ReactFragment
+      | React.ReactPortal
+      | null
+      | undefined;
+    location:
+      | boolean
+      | React.ReactChild
+      | React.ReactFragment
+      | React.ReactPortal
+      | null
+      | undefined;
+  },
+  index: React.Key | null | undefined
+) => (
+  <tr key={index}>
+    <td>{item.id}</td>
+    <td>{item.name}</td>
+    <td>{item.email}</td>
+    <td>{item.phone}</td>
+    <td>{item.total_orders}</td>
+    <td>{item.total_spend}</td>
+    <td>{item.location}</td>
+  </tr>
+);
+
+const Customers = () => {
   return (
-    <>
-      <h1>Customers</h1>
-      {children}
-    </>
+    <div>
+      <h2 className="page-header">customers</h2>
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <div className="card__body">
+              <Table
+                limit="10"
+                headData={customerTableHead}
+                renderHead={(item, index) => renderHead(item, index)}
+                bodyData={customerList}
+                renderBody={(item, index) => renderBody(item, index)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Customers;
