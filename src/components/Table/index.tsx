d03abd5445fb4757b1b58aee23eec381583f7c2b
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-10 13:10:58 -0300

    Feat: Update Pagination for Table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
+import { SetStateAction, useState } from "react";
 
 import "./styles.scss";
 
 interface TableProps {
+  limit: any[];
   headData: any[];
   renderHead: (arg0: any, arg1: any) => any;
   bodyData: any[];
@@ -10,6 +11,34 @@ interface TableProps {
 }
 
 function Table(props: TableProps) {
+  const initDataShow =
+    props.limit && props.bodyData
+      ? props.bodyData.slice(0, Number(props.limit))
+      : props.bodyData;
+
+  const [dataShow, setDataShow] = useState(initDataShow);
+
+  let pages = 1;
+
+  let range = [];
+
+  if (props.limit !== undefined) {
+    let page = Math.floor(props.bodyData.length / Number(props.limit));
+    pages = props.bodyData.length % Number(props.limit) === 0 ? page : page + 1;
+    range = [...Array(pages).keys()];
+  }
+
+  const [currPage, setCurrPage] = useState(0);
+
+  const selectPage = (page: SetStateAction<number>) => {
+    const start = Number(props.limit) * page;
+    const end = start + Number(props.limit);
+
+    setDataShow(props.bodyData.slice(start, end));
+
+    setCurrPage(page);
+  };
+
   return (
     <div className="table-wrapper">
       <table>
