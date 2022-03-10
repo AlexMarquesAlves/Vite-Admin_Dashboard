42826879849e91975cf37e26b95ea8bcfb7409d1
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-09 21:03:28 -0300

    Feat: latestOrder constant

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -122,6 +122,47 @@ const renderCustomerBody = (
   </tr>
 );
 
+const latestOrders = {
+  header: ["order id", "user", "total price", "date", "status"],
+  body: [
+    {
+      id: "#OD1711",
+      user: "john doe",
+      date: "17 Jun 2021",
+      price: "$900",
+      status: "shipping",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "$400",
+      status: "paid",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "pending",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "$400",
+      status: "paid",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "refund",
+    },
+  ],
+};
+
 function Dashboard({ children }: DashboardProps) {
   return (
     <div>
